311ab957f4530c4e7be0b877ef07e282de5c62d0
Shedrack Ajaegbu <shedrack3@gmail.com>
2022-09-13 21:39:46 -0700

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main() {
+/* This is a descriptive main */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
+
